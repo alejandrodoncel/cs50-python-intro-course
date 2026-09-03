9b7532336ed55440b34ea3d28aecdd319e56bc97
Alejandro Doncel <alejandrodonceldelgado@gmail.com>
2026-09-03 12:39:19 +0200

    day(01) - remembering basics

--- 01 Funtions and Variables/hello.py
+++ 01 Funtions and Variables/hello.py
@@ -1,6 +1,6 @@
 # Ask user for their name and age
 name = input("What's your name: ")
-age = input("What's yotu age: ")
+age = input("What's your age: ")
 
 # Say hello to the user and age
 print("Hello", name)
@@ -10,3 +10,9 @@ print("Your age is:", age)
 '''
 Multiline comment
 '''
+
+# Print function parameters
+# print(*objects, sep=' ', end='\n, file=sys.stdout, flush=False')
+
+print("Hello", name, sep='\t')
+print(f"Hello, {name}")
---
+++ 01 Funtions and Variables/string-methods.py
@@ -0,0 +1,14 @@
+name = input("What's your name: ").title()
+
+print(f'hello, {name}')
+
+# Remove whitespaces
+# name = name.strip()
+
+# Capitalize first str carather
+# name.capitalize()
+
+# Capitalize first caracther of all worlds on str
+# name.title()
+
+print(f'hello, {name.strip()}')
