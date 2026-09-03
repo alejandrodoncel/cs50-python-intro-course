6be21af87f7ca25b1375efcd165556be320dda80
Alejandro Doncel <alejandrodonceldelgado@gmail.com>
2026-09-03 11:59:28 +0200

    first commit

---
+++ 01 Funtions and Variables/hello.py
@@ -0,0 +1,12 @@
+# Ask user for their name and age
+name = input("What's your name: ")
+age = input("What's yotu age: ")
+
+# Say hello to the user and age
+print("Hello", name)
+print("Your age is:", age)
+
+# Comment example
+'''
+Multiline comment
+'''
